30d55be4ebcd516a512f9ad93fb9fe56a036ab05
KerstingJ <kersting.josh@gmail.com>
2016-11-20 11:26:44 -0600

    added templates for question and tag, added main,js

---
+++ src/main/resources/static/js/main.js
@@ -0,0 +1,8 @@
+function addAnswer(){
+    var number = document.getElementById("member").value;
+    var container = document.getElementById("container");
+ 	container.appendChild(document.createTextNode("Member ");
+ 	
+    label = document.createElement("label");
+    container.appendChild(input);
+}
